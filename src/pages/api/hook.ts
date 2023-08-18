38fa12e8ab216dbde717469ac909a329c474709a
Johny Xu <johny.xu@arkade.com.au>
2023-08-18 15:30:32 +1000

    add log

--- src/pages/api/hook.ts
+++ src/pages/api/hook.ts
@@ -23,6 +23,15 @@ function handler(req: any, res: any) {
 
   console.log("hash2:",signature === hashSignature, signature, hashSignature)
   console.log(signature.length, hashSignature.length)
+
+  for (let index = 0; index < signature.length; index++) {
+    const element = signature[index];
+    const element2 = hashSignature[index];
+    if(element !== element2){
+      console.log("index:", index, element, element2)
+    }
+  }
+
   if(signature === hashSignature){
     res.status(200).json({ success: true });
   }else{
